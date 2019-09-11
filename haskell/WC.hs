2fc6c5f5f76a8d291cb60fe00aa2994b24cb0891
Austin Erlandson <aerlandson@aspirehealthcare.com>
2019-09-10 22:18:50 -0500

    Upgraded to a Pure Monoidally composed solution ala: @chrispenner

--- haskell/WC.hs
+++ haskell/WC.hs
@@ -2,73 +2,83 @@
 
 module Main where
 
-import Prelude hiding ((>>), sum, product, min, max, lines, words)
-import System.Environment (getArgs)
+import Prelude hiding (sum, product, min, max, lines, words)
 
-import Control.Monad.State (State, evalState, get, put)
+-- import Control.Monad.State (State, evalState, get, put)
 -- import qualified Control.Foldl as L
-import Data.Char as Char
-import Data.Functor.Compose (Compose(..))
-import Data.Functor.Const (Const(..))
-import Data.Functor.Product (Product(..))
-import Data.Foldable
+import qualified Data.Char as Char
+-- import Data.Functor.Compose (Compose(..))
+-- import Data.Functor.Const (Const(..))
+-- import Data.Functor.Product (Product(..))
+-- import qualified Control.Foldl as F
+-- import qualified Control.Foldl.Text as T
+-- import Data.Foldable
+import qualified Data.Text.Lazy as L
+import qualified Data.Text.Lazy.IO as IO
 import Data.Semigroup (Sum(..))
 
 
 main :: IO ()
-main = do
-    strings <- getArgs
-    case strings of
-        [filePath] ->
-            readFile filePath
-            >>= program
-            & runProgram
-            & show
-            & putStrLn
-        _ -> pure ()
+main = IO.interact $ L.unpack & foldMap count & format & show & L.pack
 
-
-runProgram :: Product (Product (Const Count) (Const Count)) (Compose (State Bool) (Const Count)) () -> (Int, Int, Int)
-runProgram (Pair (Pair chars lines) words) =
-    ( getSum . getConst $ chars
-    , getSum . getConst $ lines
-    , getSum . getConst $ getCompose words `evalState` False
-    )
-
-
-program :: String -> Product (Product (Const Count) (Const Count)) (Compose (State Bool) (Const Count)) ()
-program = traverse_ $ Pair <$> (Pair <$> countChar <*> countLine) <*> countWords
+format :: Counts -> Report
+format (Counts chars lns (Flux _ wrds _)) =
+    Report (getSum chars) (getSum lns) (getSum wrds)
 
 type Count = Sum Int
 
-countChar :: a -> Const Count ()
-countChar = const $ Const (Sum 1)
-
-countLine :: Char -> Const Count ()
-countLine = Const . Sum . fromEnum . (== '\n')
-
-countWords :: Char -> Compose (State Bool) (Const Count) ()
-countWords c = Compose $ do
-    before <- get
-    let after = not $ Char.isSpace c
-    _ <- put after
-    pure . Const . Sum . fromEnum $ (not before && after)
+count :: Char -> Counts
+count = Counts <$> (const (Sum 1)) <*> (Sum . fromEnum . (== '\n')) <*> flux
 
 
+data Counts = Counts
+    { characters :: !Count
+    , lines :: !Count
+    , words :: !Flux
+    } deriving (Show)
+
+instance Semigroup Counts where
+     Counts charsA linesA wordsA <> Counts charsB linesB wordsB = Counts
+        (charsA <> charsB)
+        (linesA <> linesB)
+        (wordsA <> wordsB)
+
+instance Monoid Counts where
+    mempty = Counts mempty mempty mempty
+
+
+data CharType
+    = AlphaNumeric
+    | Whitespace
+    deriving (Show)
+
+data Flux = Flux
+    { left :: !CharType
+    , counter :: !Count
+    , right :: !CharType
+    } deriving (Show)
+
+instance Semigroup Flux where
+  Flux l n AlphaNumeric <> Flux AlphaNumeric n' r = Flux l (n <> n' <> Sum (-1)) r
+  Flux l n _ <> Flux _ n' r = Flux l (n <> n') r
+
+instance Monoid Flux where
+    mempty = Flux Whitespace mempty Whitespace
+
+flux :: Char -> Flux
+flux c | Char.isSpace c = mempty
+       | otherwise = Flux AlphaNumeric 1 AlphaNumeric
+
+
+data Report = Report
+    { characterCount :: Int
+    , lineCount :: Int
+    , wordCount :: Int
+    } deriving (Show)
+
+-- Helpers
 (&) :: (a -> b) -> (b -> c) -> a -> c
 (&) = flip (.)
 
--- -- Swap tupling with more readable monoid eventually.
-
--- -- data WC = WC
--- --     { characters :: (Sum Int)
--- --     , lines :: (Sum Int)
--- --     , words :: (Sum Int)
--- --     }
-
--- -- instance Semigroup WC where
--- --     (<>) (WC charsA linesA wordsA) (WC charsB linesB wordsB) =
--- --         WC (charsA <> charsB) (linesA <> linesB) (wordsA <> wordsB)
-
--- -- instance Monoid WC where
--- --     mempty = WC mempty mempty mempty
+(#) :: a -> (a -> b) -> b
+(#) = flip ($)
